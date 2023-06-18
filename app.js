b00fb31dd203e6d0e62d2e570277a767a7836da1
ManojBachu <manojbachu2001@gmail.com>
2023-06-18 02:42:42 +0000

    SQLITE_ERROR: no such column: Vishal

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+app.use(express.json());
+
+const { open } = require("sqlite");
+const sqlite3 = require("sqlite3");
+const dbpath = path.join(__dirname, "cricketTeam.db");
+let db = null;
+
+const initializeDBAndServer = async () => {
+  try {
+    db = await open({
+      filename: dbpath,
+      driver: sqlite3.Database,
+    });
+    app.listen(3000, () => {
+      console.log("Server Running at http://localhost:3000/");
+    });
+  } catch (error) {
+    console.log(`DB Error: ${error.message}`);
+    process.exit(1);
+  }
+};
+
+initializeDBAndServer();
+
+const convertDbObjectToResponseObject = (dbObject) => {
+  return {
+    playerId: dbObject.player_id,
+    playerName: dbObject.player_name,
+    jerseyNumber: dbObject.jersey_number,
+    role: dbObject.role,
+  };
+};
+
+//Get Cricket_Team Players
+
+app.get("/players/", async (request, response) => {
+  const getPlayersQuery = `
+    SELECT * FROM cricket_team;`;
+  const playersArray = await db.all(getPlayersQuery);
+  response.send(
+    playersArray.map((eachPlayer) =>
+      convertDbObjectToResponseObject(eachPlayer)
+    )
+  );
+});
+
+// POST player API
+
+app.post("/players/", async (request, response) => {
+  const playerDetails = request.body;
+  const { playerName, jerseyNumber, role } = playerDetails;
+  const addPlayerQuery = `
+    INSERT INTO cricket_team(
+    player_name,
+    jersey_number,
+    role)
+    VALUES
+    (
+        ${playerName},
+        ${jerseyNumber},
+        ${role}
+    );`;
+  const dbResponse = await db.run(addPlayerQuery);
+  const playerId = dbResponse.lastID;
+  response.send({ playerId: playerID });
+});
+
+module.exports = app;
